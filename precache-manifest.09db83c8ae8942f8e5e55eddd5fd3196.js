48412cabec237175e4abe92c52ccc7fc46397ab2
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-02 19:39:00 -0700

    Updates

---
+++ precache-manifest.09db83c8ae8942f8e5e55eddd5fd3196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb6c707f21f4294bd25e3b5b0a92734",
+    "url": "/jacobwicks.github.io/newInstructions/index.html"
+  },
+  {
+    "revision": "7d8872c28515f78ab412",
+    "url": "/jacobwicks.github.io/newInstructions/static/css/main.f0baed30.chunk.css"
+  },
+  {
+    "revision": "bc82115053852d653131",
+    "url": "/jacobwicks.github.io/newInstructions/static/js/2.2952a1d5.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/jacobwicks.github.io/newInstructions/static/js/2.2952a1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8872c28515f78ab412",
+    "url": "/jacobwicks.github.io/newInstructions/static/js/main.26566fe6.chunk.js"
+  },
+  {
+    "revision": "836d0b65e40f587a50c9",
+    "url": "/jacobwicks.github.io/newInstructions/static/js/runtime-main.adf4933f.js"
+  }
+]);
